b655a5971c11e9bc14b09862dfbb218113e4aca1
Reagan615 <419247451@qq.com>
2023-02-10 19:34:04 -0600

    create

---
+++ Introduction-to-LINQ-and-ASP.NET-MVC_Lab02/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Introduction_to_LINQ_and_ASP.NET_MVC_Lab02.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Introduction_to_LINQ_and_ASP.NET_MVC_Lab02.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
