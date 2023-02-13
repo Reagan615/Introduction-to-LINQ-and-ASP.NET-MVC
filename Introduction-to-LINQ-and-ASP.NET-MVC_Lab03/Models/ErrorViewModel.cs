517fc45adbd1485be1c214b93bbf7fb8c83763f1
Reagan615 <419247451@qq.com>
2023-02-12 21:54:23 -0600

    create

---
+++ Introduction-to-LINQ-and-ASP.NET-MVC_Lab03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Introduction_to_LINQ_and_ASP.NET_MVC_Lab03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
